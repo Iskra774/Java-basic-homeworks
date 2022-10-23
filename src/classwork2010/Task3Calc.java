ed1dd4e6fa35467bee1db6bf757511c83dbde494
Iskra774 <anton.selin@web.de>
2022-10-23 20:17:00 +0200

    homework 1level 2010 add

--- src/classwork2010/Task3Calc.java
+++ src/classwork2010/Task3Calc.java
@@ -4,11 +4,6 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Task3Calc {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(Path.of(C:\Users\selin\IdeaProjects\Javabasic\src\classwork2010\TestData.txt));
-        do {
-            String operation = scanner.next();
-        }
 
-    }
+
 }
---
+++ src/homework2010/Task3.java
@@ -0,0 +1,19 @@
+package homework2010;
+
+public class Task3 {
+    //3)распечатать треугольник из символов ^. Высоту выбирает пользователь.
+    // Например: высота = 5, на экране
+    public static void main(String[] args) {
+        int h = 5;
+        for(int i = 0; i < h;i++){
+            //for(int j = 0; j < h+3; j++){
+              //  System.out.print(" ");}
+
+            for(int j = 0; j < i*2 - 1; j++) {
+                System.out.print(">");
+            }
+            System.out.println("");
+        }
+
+    }
+}
