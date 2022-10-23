6ecc2ad6f95d7817c720c962e7174f112b501f57
Iskra774 <anton.selin@web.de>
2022-10-23 19:02:03 +0200

    classwork 2010 add

---
+++ src/classwork2010/Task3Calc.java
@@ -0,0 +1,14 @@
+package classwork2010;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Task3Calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(Path.of(C:\Users\selin\IdeaProjects\Javabasic\src\classwork2010\TestData.txt));
+        do {
+            String operation = scanner.next();
+        }
+
+    }
+}
